19045581bcf4907046544548e215eca93fc85744
cowu1 <143014727+cowu1@users.noreply.github.com>
2024-04-15 19:26:03 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,12 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    digitList = new ArrayList<Integer>();
+	    digitList = new ArrayList <Integer>();
 		if(num == 0)
 		digitList.add(0);
 		int n = num;
 		while(n>0){
-			digitList.add(0,n%10)
+			digitList.add(0,n%10);
 				n=n/10;
 		}
 	}
